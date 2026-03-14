075f6b7cac1e9f02ac8e9807c112bda27752fa90
Ali-718160 <aliearp@outlook.com>
2026-03-14 13:29:44 +0000

    Change donation variable to amount in payment intent

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const stripe = require("stripe")(process.env.SECRET_KEY);
 app.use(express.json());
 
 app.post("/create-payment-intent", async (req, res) => {
-  const donation = req.body;
+  const amount = req.body;
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: donation,
+    amount: amount,
     currency: "gbp"
   });
 
