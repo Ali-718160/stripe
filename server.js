e1c4ef1291dc76c19edb5f29885e58f254b99d03
Ali-718160 <aliearp@outlook.com>
2026-03-14 13:21:14 +0000

    Update payment intent to use donation amount in GBP

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const stripe = require("stripe")(process.env.SECRET_KEY);
 app.use(express.json());
 
 app.post("/create-payment-intent", async (req, res) => {
+  const donation = req.body;
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1000,
-    currency: "usd"
+    amount: donation,
+    currency: "gbp"
   });
 
   res.send({
