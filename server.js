d71d8ad50d9bd67987dc1977bc5b3c7af6dd56ba
Ali-718160 <aliearp@outlook.com>
2026-03-14 13:43:50 +0000

    Refactor payment intent creation and update Stripe key

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const stripe = require("stripe")(process.env.SECRET_KEY);
+const stripe = require("stripe")("sk_test_51Pe4tQ2MMIgwRw7sxP4KDClcHQ3aBC2ndMnjkxtGSXGCkd02p3UF6N7c5Si4lBJyI2iIANvUM5XOP7LU4QOXtcos00EJ0T65Yd");
 
 app.use(express.json());
 
 app.post("/create-payment-intent", async (req, res) => {
-  const amount = req.body;
+  const { amount } = req.body;
   const paymentIntent = await stripe.paymentIntents.create({
     amount: amount,
     currency: "gbp"
